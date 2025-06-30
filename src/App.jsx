6d24ec50de98b6022fac225d8baff9d9e5d422b6
sakakihase <hungnd.office@gmail.com>
2025-06-30 20:52:15 +0700

    add an interactive grid bg

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,6 @@ import "./App.css";
 import Router from "./router/router";
 import Navbar from "./components/navbar";
 
-import { useGSAP } from '@gsap/react';
-import gsap from "gsap";
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
-
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, useGSAP);
-
 function App() {
   return (
     <>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { DynamicGridPattern } from "../components/gridBackground";
 
 function Home() {
   return (
-    <div className='page-container bg-container text-primary h-[200vh]'>home boy</div>
-  )
+    <div className="page-container size-full min-h-56">
+      <div className="relative w-full h-55 overflow-hidden bg-gray-800">
+        <DynamicGridPattern numOfCols={50} numOfRows={65} />
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
